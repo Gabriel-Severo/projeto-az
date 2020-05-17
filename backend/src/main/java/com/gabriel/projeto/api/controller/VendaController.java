d2780e8d504500ac87e11c81d9d52671dd4e12c1
gabriel-severo <gabriel.ivi@hotmail.com>
2020-05-17 16:04:29 -0300

    Adicionado a rota venda

---
+++ backend/src/main/java/com/gabriel/projeto/api/controller/VendaController.java
@@ -0,0 +1,32 @@
+package com.gabriel.projeto.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gabriel.projeto.domain.exception.AutomovelVendidoException;
+import com.gabriel.projeto.domain.model.Automovel;
+import com.gabriel.projeto.domain.repository.AutomovelRepository;
+
+@RestController
+public class VendaController {
+	
+	@Autowired
+	private AutomovelRepository automovelRepository;
+	
+	@GetMapping("/venda/{automovelId}")
+	public ResponseEntity<Automovel> vender(@PathVariable Long automovelId) {
+		if(!automovelRepository.existsById(automovelId)) {
+			return ResponseEntity.notFound().build();
+		}
+		Automovel automovel = automovelRepository.findById(automovelId).get();
+		if (automovel.isVendido()) {
+			throw new AutomovelVendidoException("Automovel já vendido");
+		}
+		automovel.setVendido(true);
+		automovelRepository.save(automovel);
+		return ResponseEntity.ok(automovel);
+	}
+}
