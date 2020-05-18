1355834ef738ac4d64564a3f6e88a2c1886fe009
gabriel-severo <gabriel.ivi@hotmail.com>
2020-05-18 10:45:11 -0300

    Frontend iniciado

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Veiculos msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Veiculos from './components/Veiculos.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Veiculos
+  }
+}
+</script>
+
+<style></style>
---
+++ frontend/src/components/Veiculos.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="Veiculos">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Veiculos',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped></style>
